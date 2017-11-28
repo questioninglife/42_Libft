9ceebd8ab694436815508f4a05dca204c2e91388
Edward Chung <edchung@e1z3r5p16.42.us.org>
2017-11-28 01:12:33 -0800

    Working

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,31 +6,17 @@
 /*   By: edchung <edchung@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 15:26:14 by edchung           #+#    #+#             */
-/*   Updated: 2017/11/28 01:07:13 by edchung          ###   ########.fr       */
+/*   Updated: 2017/11/28 01:11:51 by edchung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 /*
 ** The ft_itoa() function allocates with malloc(3) and returns a "fresh"
 ** string ending with '\0' representing the integer n given as argument.
 ** Negative numbers must be supported.
 ** If the allocation fails, the function returns NULL.
-*/
-
-/*
-static void		itos(char *s, int n)
-{
-	if (n > -10 || n < 10)
-		*s++ = (n < 0 ? -n : n) + '0';
-	else
-	{
-		itos(s, n / 10);
-		*s++ = (n < 0 ? (n % 10) * -1 : n % 10) + '0';
-	}
-}
 */
 
 char			*ft_itoa(int n)
@@ -52,14 +38,4 @@ char			*ft_itoa(int n)
 		n = n / 10;
 	}
 	return (s);
-}
-
-int				main(void)
-{
-	char		*i1 = ft_itoa(-623);
-	char		*i2 = ft_itoa(156);
-	char		*i3 = ft_itoa(0);
-
-	printf("-623 %s\n 156 %s\n 0 %s\n", i1, i2, i3);
-	return (0);
 }
